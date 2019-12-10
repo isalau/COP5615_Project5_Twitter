511ed06d40be3a314c852a922fe75a54d705067f
Isabel Laurenceau <isalau@ufl.edu>
2019-12-10 18:12:02 -0500

    added some connections

--- twitter/lib/twitter_web/views/add_view.ex
+++ twitter/lib/twitter_web/views/add_view.ex
@@ -1,3 +1,9 @@
 defmodule TwitterWeb.AddView do
   use TwitterWeb, :view
+
+  def connection_keys(conn) do
+    conn
+    |> Map.from_struct()
+    |> Map.keys()
+  end
 end
--- twitter/lib/twitter_web/views/user_view.ex
+++ twitter/lib/twitter_web/views/user_view.ex
@@ -1,3 +1,9 @@
 defmodule TwitterWeb.UserView do
   use TwitterWeb, :view
+
+  def connection_keys(conn) do
+    conn
+    |> Map.from_struct()
+    |> Map.keys()
+  end
 end
