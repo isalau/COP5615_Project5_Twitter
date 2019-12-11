ec2d081d0aac2970785d74e03f2689cf294fb293
Isabel Laurenceau <isalau@ufl.edu>
2019-12-11 11:50:29 -0500

    Login Done!

--- twitter/lib/twitter_web/controllers/signincontroller.ex
+++ twitter/lib/twitter_web/controllers/signincontroller.ex
@@ -7,9 +7,30 @@ defmodule TwitterWeb.SigninController do
     |> render("index.html")
   end
 
-  def signin(conn, _params) do
-    conn
-    |> put_flash(:info, "In Signin!")
-    |> render("index.html")
+  def signin(conn, params) do
+    user_name = get_in(params, ["username"])
+    password = get_in(params, ["password1"])
+
+    {pass_users, tot_users, _, _} = :sys.get_state(:"#{Engine}_cssa")
+    # IO.inspect(tot_users, label: "Total users list in Login")
+
+    if user_name in tot_users do
+      # After checking show
+      pid_sender = :"#{user_name}"
+      {_, pass2} = List.keyfind(pass_users, pid_sender, 0)
+
+      if pass2 == password do
+        IO.puts("Successful sign in")
+
+        conn
+        |> redirect(to: Routes.user_path(conn, :index))
+      else
+        IO.puts("Failed sign in")
+
+        conn
+        |> put_flash(:info, "Incorrect username or password, plesae try again!")
+        |> render("index.html")
+      end
+    end
   end
 end
--- twitter/lib/twitter_web/router.ex
+++ twitter/lib/twitter_web/router.ex
@@ -20,6 +20,7 @@ defmodule TwitterWeb.Router do
     get "/register", RegisterController, :index
     post "/register", RegisterController, :register
     get "/signin", SigninController, :index
+    post "/signin", SigninController, :signin
     resources "/events", EventController
     resources "/tweet", TweetController
     post "/tweet", TweetController, :create
