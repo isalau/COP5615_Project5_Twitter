d6abc90e0cde079d6c1ed9a6ce3649320904f954
Isabel Laurenceau <isalau@ufl.edu>
2019-12-11 21:55:02 -0500

    fixed nil password

--- twitter/lib/twitter_web/controllers/registercontroller.ex
+++ twitter/lib/twitter_web/controllers/registercontroller.ex
@@ -13,28 +13,33 @@ defmodule TwitterWeb.RegisterController do
     user_name = get_in(params, ["username"])
     password1 = get_in(params, ["password1"])
     password2 = get_in(params, ["password2"])
-    # IO.inspect(user_name, label: "USERNAME")
     {_, all_users, _, _} = :sys.get_state(:"#{Engine}_cssa")
 
-    conn =
-      if user_name in all_users do
-        IO.puts("username already exists")
-
-        conn
-        |> put_flash(:info, "Already registered please sign in instead!")
-        |> redirect(to: Routes.page_path(conn, :index, user_name: user_name))
-      else
-        if password1 == password2 do
-          Register.reg(user_name, password1)
+    if password1 == "" do
+      conn
+      |> put_flash(:info, "Passwords cannot be nil")
+      |> render("index.html")
+    else
+      conn =
+        if user_name in all_users do
+          IO.puts("username already exists")
 
           conn
-          |> put_session(:current_user_id, user_name)
-          |> redirect(to: Routes.user_path(conn, :index, user_name: user_name))
+          |> put_flash(:info, "Already registered please sign in instead!")
+          |> redirect(to: Routes.page_path(conn, :index, user_name: user_name))
         else
-          conn
-          |> put_flash(:info, "Passwords do not match. Please try again")
-          |> render("index.html")
+          if password1 == password2 do
+            Register.reg(user_name, password1)
+
+            conn
+            |> put_session(:current_user_id, user_name)
+            |> redirect(to: Routes.user_path(conn, :index, user_name: user_name))
+          else
+            conn
+            |> put_flash(:info, "Passwords do not match. Please try again")
+            |> render("index.html")
+          end
         end
-      end
+    end
   end
 end
