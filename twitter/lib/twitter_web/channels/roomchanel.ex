5add781bf0bf0aba750e4e6f99c6bc11f23d82bc
Isabel Laurenceau <isalau@ufl.edu>
2019-12-13 08:38:51 -0500

    register with web sockets working

--- twitter/lib/twitter_web/channels/roomchanel.ex
+++ twitter/lib/twitter_web/channels/roomchanel.ex
@@ -1,12 +1,13 @@
 defmodule TwitterWeb.RoomChannel do
   use Phoenix.Channel
 
-  def join(channel_name, _params, socket) do
-    {:ok, %{channel: channel_name}, socket}
+  def join("room:" <> room_id, _params, socket) do
+    {:ok, %{channel: "room:#{room_id}"}, assign(socket, :room_id, room_id)}
   end
 
   def handle_in("message:add", %{"message" => content}, socket) do
-    broadcast!(socket, "room:lobby:new_message", %{content: content})
+    room_id = socket.assigns[:room_id]
+    broadcast!(socket, "room:#{room_id}:new_message", %{content: content})
     {:reply, :ok, socket}
   end
 end
